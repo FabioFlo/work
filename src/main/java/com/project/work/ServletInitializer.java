e2f904540099e416e111f533e83782f069ac5b0a
FabioFlo <fabio.floris@outlook.it>
2022-04-19 17:44:30 +0200

    Initial commit

---
+++ src/main/java/com/project/work/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.project.work;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectWorkApplication.class);
+	}
+
+}
